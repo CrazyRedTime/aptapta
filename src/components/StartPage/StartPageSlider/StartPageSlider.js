cfea2e40b645a79f79d560af636d67320cb20bb0
CrazyRedTime <zubbol89@gmail.com>
2021-04-16 12:14:20 +0400

    slider pagination

--- src/components/StartPage/StartPageSlider/StartPageSlider.js
+++ src/components/StartPage/StartPageSlider/StartPageSlider.js
@@ -1,12 +1,18 @@
 import Carousel from "nuka-carousel";
-import styles from './StartPageSlider.module.scss'
+import styles from "./StartPageSlider.module.scss";
 
 const StartPageSlider = () => {
   return (
     <Carousel
       width="688px"
       wrapAround={true}
-      defaultControlsConfig={{ nextButtonText: ">", nextButtonStyle: {background: 'black', height: '100%'} }}
+      defaultControlsConfig={{
+        containerClassName: styles.buttonContainer,
+        nextButtonText: ">",
+        nextButtonStyle: { background: "black", height: "100%" },
+        prevButtonText: "<",
+        prevButtonStyle: { background: "black", height: "100%" },
+      }}
     >
       <div>Slider1</div>
       <div>Slider2</div>
