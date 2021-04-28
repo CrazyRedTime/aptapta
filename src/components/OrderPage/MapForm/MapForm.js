ac3d9650fc95dff659e1859ba7eccab93b49e650
CrazyRedTime <zubbol89@gmail.com>
2021-04-28 16:01:00 +0400

    fix placeholder

--- src/components/OrderPage/MapForm/MapForm.js
+++ src/components/OrderPage/MapForm/MapForm.js
@@ -2,7 +2,7 @@ import TextField from "@material-ui/core/TextField";
 import Autocomplete from "@material-ui/lab/Autocomplete";
 import { createFilterOptions } from "@material-ui/lab/Autocomplete";
 
-import styles from './MapForm.module.scss'
+import styles from "./MapForm.module.scss";
 
 const MapForm = ({
   citiesFromState,
@@ -14,7 +14,6 @@ const MapForm = ({
   setZoom,
   setCurrentAddress,
 }) => {
-
   const filterOptionsForCity = createFilterOptions({
     matchFrom: "start",
     stringify: (option) => option.name,
@@ -23,23 +22,30 @@ const MapForm = ({
   const filterOptionsForAddress = createFilterOptions({
     matchFrom: "start",
     stringify: (option) => option.address,
-    startAfter: 2
+    startAfter: 2,
   });
 
   return (
     <div className={styles.mapForm}>
       <Autocomplete
         id="city-select"
-        classes={{
-          popupIndicator: styles.none,
-        }}
+        forcePopupIcon={false}
         options={citiesFromState}
         getOptionLabel={(option) => option.name}
         style={{ width: 300 }}
         getOptionSelected={(option, value) => option.id === value.id}
         filterOptions={filterOptionsForCity}
-        renderInput={(params) => <TextField {...params} label="Город" placeholder="Начните вводить город..." />}
-        noOptionsText='Ничего не найдено'
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Город"
+            placeholder="Начните вводить город..."
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+        )}
+        noOptionsText="Ничего не найдено"
         onChange={(event, value, reason) => {
           if (reason === "select-option") {
             setCenter({ lat: value.lat, lng: value.lng });
@@ -57,16 +63,23 @@ const MapForm = ({
       />
       <Autocomplete
         id="point-select"
-        classes={{
-          popupIndicator: styles.none,
-        }}
+        forcePopupIcon={false}
         options={filteredMarkers}
         getOptionLabel={(option) => option.address}
         filterOptions={filterOptionsForAddress}
         style={{ width: 300 }}
         getOptionSelected={(option, value) => option.id === value.id}
-        renderInput={(params) => <TextField {...params} label="Пункт выдачи" placeholder="Начните вводить пункт..." />}
-        noOptionsText='Ничего не найдено'
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Пункт выдачи"
+            placeholder="Начните вводить пункт..."
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+        )}
+        noOptionsText="Ничего не найдено"
         onChange={(event, value, reason) => {
           if (reason === "select-option") {
             setCenter({ lat: value.lat, lng: value.lng });
